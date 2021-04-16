775289d20bcaa2731700371f85265eab23c42381
Ibrahim Nergiz <phyesix@gmail.com>
2021-04-16 23:38:46 +0300

    update

--- pageScraper.js
+++ pageScraper.js
@@ -1,6 +1,35 @@
+const mariadb = require('mariadb');
+require('dotenv').config()
+
+const cpool = mariadb.createPool({
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER, 
+  password: process.env.DB_PWD,
+  database: process.env.DB_DB
+});
+
+let connection; 
+
 const scraperObject = {
+  urlList: [
+    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-nisan-2021.html',
+    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-mart-2021.html',
+    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-subat-2021.html',
+    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-ocak-2021.html',
+    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-aralik-2020.html',
+    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-kasim-2020.html',
+    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-ekim-2020.html',
+    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-agustos-2020.html',
+    'https://www.mebehliyetsinavsorulari.com/temmuz-2020-ehliyet-sinav-sorulari.html',
+    'https://www.mebehliyetsinavsorulari.com/haziran-2020-ehliyet-sinav-sorulari.html',
+    'https://www.mebehliyetsinavsorulari.com/mayis-2020-ehliyet-sinav-sorulari.html',
+    'https://www.mebehliyetsinavsorulari.com/nisan-2020-ehliyet-sinav-sorulari.html',
+    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-mart-2020.html',
+  ],
   url: 'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-nisan-2021.html',
   async scraper(browser){
+    connection = await cpool.getConnection();
+
     let page = await browser.newPage();
     console.log(`Navigating to ${this.url}...`);
 
@@ -28,15 +57,16 @@ const scraperObject = {
         for (let i = 1; i < questionCount+1; i++) {
           await newPage.waitForSelector('#mtq_question-'+i+'-1 .mtq_question_text');
 
-          let question = await newPage.$eval('#mtq_question-'+i+'-1 .mtq_question_text', text => text.textContent);
+          let question = await newPage.$eval('#mtq_question-'+i+'-1 .mtq_question_text', text => text.innerHTML);
           let answers = await newPage.$eval('#mtq_question-'+i+'-1 .mtq_answer_table', (el) => {
             let answersArray = [];
             for (let i = 0; i < el.getElementsByClassName("mtq_answer_text").length; i++) {
-              let element = el.getElementsByClassName("mtq_answer_text")[i].textContent.trim();
+              let element = el.getElementsByClassName("mtq_answer_text")[i].innerHTML.trim();
               answersArray.push(element);
             }
             return answersArray;
           });
+          
           let rightAnswer = await newPage.$eval('#mtq_question-'+i+'-1 .mtq_correct_marker', el => el.parentElement.firstElementChild.textContent);
           let dataObj = {
             question: question,
@@ -44,7 +74,12 @@ const scraperObject = {
             rightAnswer: rightAnswer
           }
 
-          console.log("dataObj", dataObj);
+          let s = JSON.stringify(dataObj);
+          console.log('dataObj sssss: ' + s);
+
+          await connection.query('INSERT INTO questions SET data=?', s, function (error, results, fields) {
+            if (error) throw error;
+          });
         }
 
         resolve(dataObj);
@@ -56,6 +91,8 @@ const scraperObject = {
       // scrapedData.push(currentPageData);
       console.log(currentPageData);
     }  
+
+
   }
 }
 

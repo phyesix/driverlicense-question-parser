bc79c429b43fb753fb08c1d06904b03010995a26
Ibrahim Nergiz <phyesix@gmail.com>
2021-04-23 17:50:14 +0300

    URL update

--- pageScraper.js
+++ pageScraper.js
@@ -12,14 +12,14 @@ let connection;
 
 const scraperObject = {
   urlList: [
-    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-nisan-2021.html',
-    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-mart-2021.html',
-    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-subat-2021.html',
-    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-ocak-2021.html',
-    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-aralik-2020.html',
-    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-kasim-2020.html',
-    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-ekim-2020.html',
-    'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-agustos-2020.html',
+    // 'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-nisan-2021.html',
+    // 'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-mart-2021.html',
+    // 'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-subat-2021.html',
+    // 'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-ocak-2021.html',
+    // 'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-aralik-2020.html',
+    // 'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-kasim-2020.html',
+    // 'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-ekim-2020.html',
+    // 'https://www.mebehliyetsinavsorulari.com/ehliyet-sinav-sorulari-agustos-2020.html',
     'https://www.mebehliyetsinavsorulari.com/temmuz-2020-ehliyet-sinav-sorulari.html',
     'https://www.mebehliyetsinavsorulari.com/haziran-2020-ehliyet-sinav-sorulari.html',
     'https://www.mebehliyetsinavsorulari.com/mayis-2020-ehliyet-sinav-sorulari.html',
@@ -31,68 +31,71 @@ const scraperObject = {
     connection = await cpool.getConnection();
 
     let page = await browser.newPage();
-    console.log(`Navigating to ${this.url}...`);
+    
 
-    await page.goto(this.url)
-    await page.waitForSelector('#content');
+    for (let pi = 0; pi < this.urlList.length; pi++) {
+      await page.goto(this.urlList[pi]);
+      
+      console.log(`Navigating to ${this.urlList[pi]}...`);
+      
+      await page.waitForSelector('#content');
 
-    let urls = await page.$$eval('tbody tr a', links => {
-        links = links.map(el => el.href)
-        return links;
-    });
+      let urls = await page.$$eval('tbody tr a', links => {
+          links = links.map(el => el.href)
+          return links;
+      });
 
-    let pagePromise = (link) => new Promise(async(resolve, reject) => {
-        let dataObj = {};
-        let newPage = await browser.newPage();
+      let pagePromise = (link) => new Promise(async(resolve, reject) => {
+          let dataObj = {};
+          let newPage = await browser.newPage();
 
-        await newPage.goto(link);
-        await newPage.waitForSelector('.items');
+          await newPage.goto(link);
+          await newPage.waitForSelector('.items');
 
-        let questionCount = await newPage.evaluate(() => {
-          return document.querySelectorAll('.mtq_question').length;
-        });
-
-        console.log("questionCount", questionCount);
-
-        for (let i = 1; i < questionCount+1; i++) {
-          await newPage.waitForSelector('#mtq_question-'+i+'-1 .mtq_question_text');
-
-          let question = await newPage.$eval('#mtq_question-'+i+'-1 .mtq_question_text', text => text.innerHTML);
-          let answers = await newPage.$eval('#mtq_question-'+i+'-1 .mtq_answer_table', (el) => {
-            let answersArray = [];
-            for (let i = 0; i < el.getElementsByClassName("mtq_answer_text").length; i++) {
-              let element = el.getElementsByClassName("mtq_answer_text")[i].innerHTML.trim();
-              answersArray.push(element);
-            }
-            return answersArray;
+          let questionCount = await newPage.evaluate(() => {
+            return document.querySelectorAll('.mtq_question').length;
           });
-          
-          let rightAnswer = await newPage.$eval('#mtq_question-'+i+'-1 .mtq_correct_marker', el => el.parentElement.firstElementChild.textContent);
-          let dataObj = {
-            question: question,
-            answers: answers,
-            rightAnswer: rightAnswer
+
+          console.log("questionCount", questionCount);
+
+          for (let i = 1; i < questionCount+1; i++) {
+            await newPage.waitForSelector('#mtq_question-'+i+'-1 .mtq_question_text');
+
+            let question = await newPage.$eval('#mtq_question-'+i+'-1 .mtq_question_text', text => text.innerHTML);
+            let answers = await newPage.$eval('#mtq_question-'+i+'-1 .mtq_answer_table', (el) => {
+              let answersArray = [];
+              for (let i = 0; i < el.getElementsByClassName("mtq_answer_text").length; i++) {
+                let element = el.getElementsByClassName("mtq_answer_text")[i].innerHTML.trim();
+                answersArray.push(element);
+              }
+              return answersArray;
+            });
+            
+            let rightAnswer = await newPage.$eval('#mtq_question-'+i+'-1 .mtq_correct_marker', el => el.parentElement.firstElementChild.textContent);
+            let dataObj = {
+              question: question,
+              answers: answers,
+              rightAnswer: rightAnswer
+            }
+
+            let s = JSON.stringify(dataObj);
+            console.log('dataObj sssss: ' + s);
+
+            await connection.query('INSERT INTO questions SET data=?', s, function (error, results, fields) {
+              if (error) throw error;
+            });
           }
 
-          let s = JSON.stringify(dataObj);
-          console.log('dataObj sssss: ' + s);
+          resolve(dataObj);
+          await newPage.close();
+      });
 
-          await connection.query('INSERT INTO questions SET data=?', s, function (error, results, fields) {
-            if (error) throw error;
-          });
-        }
-
-        resolve(dataObj);
-        await newPage.close();
-    });
-
-    for(link in urls){
-      let currentPageData = await pagePromise(urls[link]);
-      // scrapedData.push(currentPageData);
-      console.log(currentPageData);
-    }  
-
-
+      for(link in urls){
+        let currentPageData = await pagePromise(urls[link]);
+        // scrapedData.push(currentPageData);
+        console.log(currentPageData);
+      }  
+    }
   }
 }
 
